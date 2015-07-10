a1ac002ed9b214b792454029d54d0437126618ad
kalaichellamuthu <kchellamuthu@inadco.com>
2015-07-10 00:07:01 -0700

    Update RemoveCharacter.java

--- InterviewQuestion/Tests/RemoveCharacter.java
+++ InterviewQuestion/Tests/RemoveCharacter.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.testng.annotations.Test;
 
 public class RemoveCharacter {
+	//Positive Scenario
 	@Test
 	public void test() {
 	RemoveCharacter char1= new RemoveCharacter();
@@ -12,7 +13,16 @@ public class RemoveCharacter {
         String actual=char1.removeChar1("Heo");
 	assertEquals(expected,actual);
 	}
+	//Negative scenario
+	@Test
+	public void test1() {
+	RemoveCharacter char1= new RemoveCharacter();
+	String expected="Heo";
+        String actual=char1.removeChar1("Hello");
+	assertEquals(expected,actual);
+	}
     }
+    
 
   
 
